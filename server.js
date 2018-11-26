1f706883f2b66a38f9cf7a0c8aa353b7185293c1
AndréSDS <andre_smiths@outlook.com>
2018-11-25 23:22:31 -0200

    criando conexão com banco de dados usando mongoose

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict'
-
+//importar mongoose
+const mongoose = require ("mongoose");
 //import express
 const Express = require ('express');
+//importa arquivo client-controller.js
 const ClientController = require('./controller/client-controller');
 
 //classe servidor
@@ -9,6 +11,14 @@ class Server {
   constructor(){
     //instância do express
     this.app = new Express();
+    //conectar com db mlab
+    mongoose.connect('mongodb://rammpk:rammpk5@ds231133.mlab.com:31133/hightechcrud');
+
+    const Cat = mongoose.model('Cat', {name: String });
+
+    const kitty = new Cat({name: "andre"});
+    kitty.save().then( () => console.log("Cheguei")).catch( (err) => {throw err});
+    
     //intancia o ClientController
     this.ClientController = new ClientController(this.app);
 
