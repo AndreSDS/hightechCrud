eac166b9c5eb619084b545b8d347b8dcf361e28b
AndréSDS <andre_smiths@outlook.com>
2018-12-20 00:33:25 -0200

    acertando detalhes no frontend

--- crud-front-pwa/src/components/Client.vue
+++ crud-front-pwa/src/components/Client.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
-    <v-layout>
+    <v-layout row>
 
-      <v-flex md10 lg8 xs12 offset-lg2 offset-md1> 
+      <v-flex xs12 md10 offset-md1> 
         <v-form ref="form">
         <v-text-field
           label='Nome' required 
@@ -19,12 +19,22 @@
           v-model="client.city"
         ></v-text-field>
 
-        <v-btn color="primary" @click="save()">
-          Salvar
-        </v-btn>
-        <v-btn color="success" @click="clean()">
-          Limpar
-        </v-btn>
+        <v-flex xs12 class="text-xs-center">
+          <v-btn 
+            color="info"
+            :loading = "saving" 
+            :disabled = "saving" 
+            @click="save()"
+            >
+            Salvar
+          </v-btn>
+          <v-btn 
+            color="success" 
+            @click="clean()"
+            >
+            Limpar
+          </v-btn>
+        </v-flex>
       </v-form>
 
       <template>
@@ -53,6 +63,17 @@
       </template>
       </v-flex>
     </v-layout>
+    <v-snackbar
+      :timeout="3000" 
+      :top="true" 
+      :right="true"
+      :multi-line="true"
+      :vertical="false"
+      v-model ="snackbar.show"
+      >
+        {{ snackbar.text }}
+      <v-btn flat color="pink" @click.native="snackbar.show = false">Close</v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -63,7 +84,9 @@ import axios from 'axios';
 export default {
     data(){
       return {
+        saving : false,
         client: {},
+        snackbar: { show: false, text: "" },
         headers: [
             {
                 text: "Nome",
@@ -79,6 +102,10 @@ export default {
       }
     },
     methods: {
+      showSnackbar(text){
+        this.snackbar.text =text;
+        this.snackbar.show = true;
+      },
         async findAll(){
           try{
             let resp = await axios.get('http://localhost:3000/clients');
@@ -89,6 +116,7 @@ export default {
           }
         },
         async save(){
+          this.saving = true;
             try {
               if (this.client._id) {
                 let resp = await axios.put("http://localhost:3000/clients", this.client);
@@ -96,11 +124,14 @@ export default {
                 let resp = await axios.post("http://localhost:3000/clients", this.client); 
               }
               this.findAll();
-              this.clean();         
+              this.clean();
+              this.showSnackbar("Salvo com sucesso.");
             } catch (error) {
               alert("Erro ao salvar cliente.");
               console.log("Erro ao cadastrar cliente: "+ error);
-              }
+            } finally{
+              this.saving = false;
+            }
         },
         async edit(client){
           this.client = client;
